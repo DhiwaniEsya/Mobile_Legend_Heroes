01990ad8f8f8227bb5724122e5cf013626a0d31d
Dhiwani Maharani Aulia Nur Esa Dhiwani Maharani Aulia Nur Esa <dhiwaniesya28@gmail.com>
2018-12-12 06:51:55 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {TabNavigator} from 'react-navigation';
+
+import {Home} from './Components/Home';
+import {Heroes} from './Components/Heroes';
+import {Settings} from './Components/Settings';
+
+const Tab = TabNavigator({
+  Home: {
+    screen: Home,
+  },
+  Heroes: {
+    screen: Heroes,
+  },
+  Settings: {
+    screen: Settings,
+  }
+},{
+  tabBarPosition: 'bottom',
+  swipeEnabled: true,
+  tabBarOptions:{
+    activeTintColor: '#f8f8f8',
+    activeBackgroundColor: '#586589',
+    inactiveTintColor: '#1234',
+    style:{
+      backgroundColor:'#000055',
+    },
+
+    labelStyle: {
+      fontSize: 10,
+      color: 'white',
+      padding: 5,
+    }
+  }
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Tab/>
+    );
+  }
+}
